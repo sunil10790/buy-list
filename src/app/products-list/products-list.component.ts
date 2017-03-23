f61474b98cbdf9fba1614653cd7f28747485ca47
Sunil <sunil10790@gmail.com>
2017-03-23 08:18:12 +0530

    List the products using ngFor

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,56 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'bl-products',
-  templateUrl: 'app/products-list/products-list.component.html'
+    selector: 'bl-products',
+    templateUrl: 'app/products-list/products-list.component.html'
 })
-export class ProductsListComponent  { 
-  pageTitle : string = 'Product List';
-  products: any[] = [
-    {
-        "productId": 1,
-        "productName": "Leaf Rake",
-        "releaseDate": "March 19, 2016",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "desperationLevel": 3.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-    },
-    {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "releaseDate": "March 18, 2016",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "desperationLevel": 4.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-    },
-    {
-        "productId": 5,
-        "productName": "Hammer",
-        "releaseDate": "May 21, 2016",
-        "description": "Curved claw steel hammer",
-        "price": 8.9,
-        "desperationLevel": 4.8,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-    },
-    {
-        "productId": 8,
-        "productName": "Saw",
-        "releaseDate": "May 15, 2016",
-        "description": "15-inch steel blade hand saw",
-        "price": 11.55,
-        "desperationLevel": 3.7,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
-    },
-    {
-        "productId": 10,
-        "productName": "Video Game Controller",
-        "releaseDate": "October 15, 2015",
-        "description": "Standard two-button video game controller",
-        "price": 35.95,
-        "desperationLevel": 4.6,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
-    }
-]
+export class ProductsListComponent {
+    pageTitle: string = 'Product List';
+    products: any[] = [
+        {
+            "productId": 1,
+            "productName": "Harley Davidson IRON 883",
+            "description": "Aggressive throwback styling taken to a place altogether new. No need to shine this machine. Just get on and tear up the nearest street.",
+            "price": 800000,
+            "desperationLevel": 9.5,
+            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+        },
+        {
+            "productId": 2,
+            "productName": "Flute",
+            "description": "Bansuri Flute",
+            "price": 3000,
+            "desperationLevel": 6,
+            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+        }
+    ]
 }

8fcb7e388903145fd8cf74b2c3ed5b14bf40f9f0
Sunil <sunil10790@gmail.com>
2017-03-23 07:46:20 +0530

    Add products-list component and include bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { ProductsListComponent }  from './products-list/products-list.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent , ProductsListComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/products-list/products-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bl-products',
+  templateUrl: 'app/products-list/products-list.component.html'
+})
+export class ProductsListComponent  { 
+  pageTitle : string = 'Product List';
+}
